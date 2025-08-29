ad03b1f9349420c79ea39a1ecc206788fab6ecd4
ebincodes <ebin.codes@gmail.com>
2025-08-30 04:05:33 +0530

    Minor change

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -7,7 +7,6 @@ import { Repository } from '../services/repositoryService';
 
 const { Title, Paragraph } = Typography;
 
-
 const Subscriptions: React.FC = () => {
   const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
   const [loading, setLoading] = useState(true);
